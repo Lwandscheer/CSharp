aadc771806bd6e4493ed18a354450e43b56f3219
Leonardo Wandscheer <lwandscheer@gmail.com>
2022-03-28 17:49:53 -0300

    fixing

---
+++ classcases/polimorfismo/Program.cs
@@ -0,0 +1,28 @@
+﻿// Atribuindo instância
+Pagamento pagamento01 = new PagamentoViaTed();
+Pagamento pagamento02 = new PagamentoViaBoleto();
+
+pagamento01.Pagar();
+pagamento02.Pagar();
+
+
+// Classe base
+public class Pagamento 
+{
+    public double Valor {get; set;}
+    public DateTime Vencimento {get; set;}
+
+    public virtual void Pagar() {}
+}
+
+// As duas classes que herdaram os campos da base usam os modificadores override para definir suas próprias definições
+public class PagamentoViaBoleto : Pagamento
+{
+    public override void Pagar() => System.Console.WriteLine("Pagamento por boleto");
+}
+
+public class PagamentoViaTed : Pagamento
+{
+    public override void Pagar() => System.Console.WriteLine("Pagamento via Ted");
+}
+
