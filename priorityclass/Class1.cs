a0856e949bae7b1de97c1992b523e7feea75ec2d
Leonardo Wandscheer <lwandscheer@gmail.com>
2022-03-28 04:04:06 -0300

    classes

---
+++ priorityclass/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace priorityclass;
+    public class Class1
+    {
+        public virtual void drawline() => System.Console.WriteLine("linha");
+        public virtual void drawpoint() => System.Console.WriteLine("point");
+
+        // adição posterior de um método semelhante ao drawrectangle
+        public virtual void drawrectangle() => System.Console.WriteLine("Rectangle by Main");
+    }
+
+
+
+    public class DerivedClass : Class1
+    {
+        // a classe derivada cria um novo método ao sistema
+        public static void drawrectangle() => System.Console.WriteLine("Rectangle"); 
+        // Sem o modificador new ou override, o compilador gerará um erro e escolherá esse método como substituto
+        // métodos estáticos não suportam o modificador override, por algum motivo
+        // modificador new, após o modificador está é aceito pelo compilador
+        // Em métodos não estático, usa-se o modelo padrão
+    }
+
+
+// Esses modificadores podem ser utilizados em propriedades, indexadores e eventos
+
+// Override --> modificador de preferência
+// New --> modificador de substituição (o método escolhido será substituido)
+
+// Virtual --> o modificador virtual permite a implementação de uma nova versão do método
+// A criação de uma nova drawrectangle no derivado ocorre sobre permissão do Virtual
+// Métodos, por padrão, não são virtuais. A aplicação desse modificador deve ser feita na base
